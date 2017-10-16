07da6c965c6d709e3fba19b3295d37e33a466456
Ryan Linton <ryan@infinite.red>
2017-10-16 10:45:56 -0700

    Fix tests

--- test/add.js
+++ test/add.js
@@ -5,13 +5,19 @@ const plugin = require('../plugin')
 test('adds the proper npm module', async t => {
   // spy on few things so we know they're called
   const addModule = sinon.spy()
+  const copy = sinon.spy()
+  const exists = sinon.spy()
+  const patchInFile = sinon.spy()
 
   // mock a context
   const context = {
-    ignite: { addModule }
+    ignite: { addModule, patchInFile },
+    filesystem: { copy, exists }
   }
 
   await plugin.add(context)
 
   t.true(addModule.calledWith('redux-persist'))
+  t.true(addModule.calledWith('ramda'))
+  t.true(addModule.calledWith('seamless-immutable'))
 })
--- test/remove.js
+++ test/remove.js
@@ -3,10 +3,17 @@ const sinon = require('sinon')
 const plugin = require('../plugin')
 
 test('removes redux persist', async t => {
+  const confirm = sinon.spy()
+  const exists = sinon.spy()
+  const remove = sinon.spy()
   const removeModule = sinon.spy()
+  const patchInFile = sinon.spy()
 
+  // mock a context
   const context = {
-    ignite: { removeModule }
+    ignite: { removeModule, patchInFile },
+    filesystem: { remove, exists },
+    prompt: { confirm }
   }
 
   await plugin.remove(context)
