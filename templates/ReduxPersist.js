a70a653b17de9315e398cb1be711c814cee69e9b
Ryan Linton <ryanlntn@gmail.com>
2017-12-20 09:44:07 -0800

    Active by default now that we're a plugin (#7)

--- templates/ReduxPersist.js
+++ templates/ReduxPersist.js
@@ -3,7 +3,7 @@ import { AsyncStorage } from 'react-native'
 
 // More info here:  https://shift.infinite.red/shipping-persistant-reducers-7341691232b1
 const REDUX_PERSIST = {
-  active: false,
+  active: true,
   reducerVersion: '1.0',
   storeConfig: {
     key: 'primary',
